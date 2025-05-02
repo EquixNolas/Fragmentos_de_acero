48acb3bbe23166fe4f2bebe70b730d28daee21d9
Christopher Nolasco <chris03valerioo@gmail.com>
2025-05-02 17:19:53 +0200

    Preparing Jump Wall
    Preparando el Jump Wall para saltos

---
+++ Assets/Scripts/CanDoubleJump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanDoubleJump : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMovement pm = other.GetComponent<PlayerMovement>();
+            if (pm != null)
+            {
+                pm.canDoubleJump = true; //Desbloquea la habilidad
+                Debug.Log("Double Jump unlocked!");
+            }
+            Destroy(gameObject); // Elimina el pickup tras recogerlo
+        }
+    }
+}
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,45 +8,47 @@ public class PlayerMovement : MonoBehaviour
     Animator animator;
 
     //Checking ground
-    [Header("Ground Check")]
+    [Header("Ground Check")]//Titulo de la sección
     [SerializeField] TrailRenderer dashTrail; // Prefab del trail del dash
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck; //Empty object que verifica si estas en el suelo
+    [SerializeField] LayerMask groundLayer; //Layer del Suelo
     //[SerializeField] GameObject dashGhostPrefab;
 
-    SpriteRenderer spriteRenderer;
+    SpriteRenderer spriteRenderer; //Renderizador del sprite
 
-    float move;
-    [Header("Movement")]
-    [SerializeField]float speed = 5f;
-    bool lookDch = true;
+    float move; //Variable para el movimiento
+    [Header("Movement")] //sección de movimiento
+    [SerializeField]float speed = 5f; //variable para la velocidad
+    bool lookDch = true; //Variable para mirar a la derecha
 
-    int availableJumps;
-    [Header("Jump")]
-    [SerializeField]float jumpForce = 10f;
-    [SerializeField] int totalExtraJumps = 2;
-    [SerializeField]float fallMultiplier = 1.2f;
-    float normalGravityScale;
-    
-    [Header("Propulsion")]
-    [SerializeField]float planeoSpeed = 2f;
-    bool isPropulsing = false;
-   
-    [Header("Dash")]
+    int availableJumps; //saltos disponibles
+    [Header("Jump")] //sección de salto
+    [SerializeField]float jumpForce = 10f; //Fuerza del salto
+    [SerializeField] int totalExtraJumps = 2; //Saltos extra 
+    [SerializeField]float fallMultiplier = 1.2f; //Multiplicador de gravedad para caer más rápido
+    public bool canDoubleJump = false; //Variable para el doble salto
+    float normalGravityScale; //Gravedad normal del objeto
+
+    [Header("Propulsion")] //sección de propulsión
+    [SerializeField]float planeoSpeed = 2f; //Velocidad de planeo
+    bool isPropulsing = false; //Variable para saber si se está planeando
+    bool canPropulse = false; //Variable para saber si se puede planear
+
+    [Header("Dash")] //sección de dash
     [SerializeField] float dashForce = 15f;       // Fuerza del dash
     [SerializeField] float dashTime = 0.2f;   // Duración del dash
     [SerializeField] float dashCooldown = 0.5f;   // Tiempo entre dashes
 
-    bool isDashing = false;
-    bool canDash = true;
+    bool isDashing = false; //Variable para saber si se está haciendo dash
+    bool canDash = true; //Variable para saber si se puede hacer dash
 
-    Vector2 dashInput;
-    Vector2 vecGravity;
+    Vector2 dashInput; // Variable para la dirección del dash
+    Vector2 vecGravity; //Vector2 de gravedad
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); //
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
@@ -115,13 +117,121 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+
+    //ACCIONES DE PERSONAJE
+    void Mover()
+    {
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        Girar();
+    }
+
+    void Girar()
+    {
+        if (move < -0.1 && lookDch)
+        {
+            flip();
+        }
+
+        if (move > 0.1 && !lookDch)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        lookDch = !lookDch;
+        /*
+        if (lookDch == true)
+            spriteRenderer.flipX = false;
+        else
+            spriteRenderer.flipX = true;
+        */
+
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+
+    void DoJump()
+    {
+        if (isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // Reset vertical velocity
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+
+            animator.SetBool("saltar", true);
+            animator.SetBool("fall", false);
+
+
+            availableJumps = totalExtraJumps;
+        }
+
+        else if (availableJumps > 0 && canDoubleJump)
+        {
+            animator.SetBool("fall", true);
+
+            float force = jumpForce;
+            if (Mathf.Abs(move) > 0.1f) force *= 0.7f;
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // Reset vertical velocity
+            rb.AddForce(Vector3.up * force/1.2f, ForceMode2D.Impulse);
+            
+
+            animator.SetBool("saltar", true);
+            
+            availableJumps--;
+        }
+    }
+    void fall()
+    {
+
+        if (!isGrounded () && rb.velocity.y < -0.1f  /* && !isPropulsing*/)
+        {
+            animator.SetBool("fall", true);     
+            rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
+
+        }
+
+        else
+        {
+            animator.SetBool("fall", false);
+
+        }
+    }
+
+    void Propulsion()
+    {
+        if (isPropulsing && !isGrounded() && rb.velocity.y < 0 && canPropulse)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -planeoSpeed));
+            /*
+            rb.gravityScale = 0f;
+
+            if(rb.velocity.y < maxPropulsionSpeed)
+            {
+                rb.AddForce(Vector2.up * propulsionForce, ForceMode2D.Force);
+            }
+           
+            /*
+            rb.AddForce(Vector2.up * 500f * Time.deltaTime, ForceMode2D.Force);
+            Debug.Log("Propulsando");
+            */
+        }
+        else
+        {
+            rb.gravityScale = normalGravityScale;
+        }
+    }
+
+
     public void Dash(InputAction.CallbackContext context)
     {
         if (context.started && canDash)
         {
             Vector2 direcion = dashInput;
 
-            if(direcion == Vector2.zero)
+            if (direcion == Vector2.zero)
             {
                 direcion = lookDch ? Vector2.right : Vector2.left;
             }
@@ -149,12 +259,13 @@ public class PlayerMovement : MonoBehaviour
                 Instantiate(dashGhostPrefab, transform.position, Quaternion.identity);
             }
          */
-            dashTrail.emitting = true;  
+            dashTrail.emitting = true;
             elapsedTime += 0.05f;
             yield return new WaitForSeconds(0.05f);
         }
 
         rb.gravityScale = normalGravityScale;
+        dashTrail.emitting = false;
         isDashing = false;
 
         yield return new WaitForSeconds(dashCooldown);
@@ -163,113 +274,6 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-   
-
-    //ACCIONES DE PERSONAJE
-    void Mover()
-    {
-        rb.velocity = new Vector2(move * speed, rb.velocity.y);
-
-        Girar();
-    }
-
-    void Girar()
-    {
-        if (move < -0.1 && lookDch)
-        {
-            flip();
-        }
-
-        if (move > 0.1 && !lookDch)
-        {
-            flip();
-        }
-    }
-
-    void flip()
-    {
-        lookDch = !lookDch;
-
-        if (lookDch == true)
-            spriteRenderer.flipX = false;
-        else
-            spriteRenderer.flipX = true;
-
-        //Vector3 escala = transform.localScale;
-        //escala.x *= -1;
-        //transform.localScale = escala;
-    }
-
-    void DoJump()
-    {
-        if (isGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0f); // Reset vertical velocity
-            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-
-            animator.SetBool("saltar", true);
-            animator.SetBool("fall", false);
-
-
-            availableJumps = totalExtraJumps;
-        }
-
-        else if (availableJumps > 0)
-        {
-            animator.SetBool("fall", true);
-
-            float force = jumpForce;
-            if (Mathf.Abs(move) > 0.1f) force *= 0.7f;
-            rb.velocity = new Vector2(rb.velocity.x, 0f); // Reset vertical velocity
-            rb.AddForce(Vector3.up * force/1.2f, ForceMode2D.Impulse);
-            
-
-            animator.SetBool("saltar", true);
-            
-            availableJumps--;
-        }
-    }
-    void fall()
-    {
-
-        if (!isGrounded () && rb.velocity.y < -0.1f  /* && !isPropulsing*/)
-        {
-            animator.SetBool("fall", true);     
-            rb.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
-            dashTrail.emitting = false;
-
-        }
-
-        else
-        {
-            animator.SetBool("fall", false);
-
-        }
-    }
-
-    void Propulsion()
-    {
-        if (isPropulsing && !isGrounded() && rb.velocity.y < 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -planeoSpeed));
-            /*
-            rb.gravityScale = 0f;
-
-            if(rb.velocity.y < maxPropulsionSpeed)
-            {
-                rb.AddForce(Vector2.up * propulsionForce, ForceMode2D.Force);
-            }
-           
-            /*
-            rb.AddForce(Vector2.up * 500f * Time.deltaTime, ForceMode2D.Force);
-            Debug.Log("Propulsando");
-            */
-        }
-        else
-        {
-            rb.gravityScale = normalGravityScale;
-        }
-    }
 
     //COLLISIONES
     private void OnCollisionEnter2D(Collision2D collision)
